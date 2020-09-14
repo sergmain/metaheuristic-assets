6ff3b33978f2a6817d1f39016c50fae03d4943ed
sergmain <serg_main@yahoo.com>
2020-09-14 14:46:19 -0700

    .

---
+++ curls/curl-exec-context-to-producing.cmd
@@ -0,0 +1,1 @@
+curl -u q:123 http://localhost:8080/rest/v1/dispatcher/source-code/exec-context-target-exec-state/1/producing/%1
--- curls/curl-workbook-to-producing.cmd
+++
@@ -1,1 +0,0 @@
-curl -u q:123 http://localhost:8080/rest/v1/launchpad/plan/workbook-target-exec-state/1/producing/%1
