51c4d74bab5340cb952af565da540aaa4796632c
sergmain <serg_main@yahoo.com>
2023-11-12 23:37:25 -0800

    .

--- examples/simple-metrics/curl-add-source-code.cmd
+++ examples/simple-metrics/curl-add-source-code.cmd
@@ -1,1 +1,1 @@
-curl -u q:123 -F "file=@simple-metrics-plan.yaml"  http://localhost:8080/rest/v1/launchpad/plan/plan-upload-from-file
+curl -u q:123 -F "file=@simple-metrics-plan.yaml"  http://localhost:8080/rest/v1/dispatcher/source-code/source-code-upload-from-file
